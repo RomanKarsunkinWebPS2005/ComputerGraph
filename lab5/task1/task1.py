87f5cabaed821ef44b8543cdb0d9595d3076ccb1
Roman <hozgllox@gmail.com>
2026-09-07 00:09:21 +0300

    lab5

---
+++ lab5/task1/task1.py
@@ -0,0 +1,387 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QOpenGLWidget
+from PyQt5.QtCore import QTimer, Qt
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from PIL import Image
+from math import sin, cos, pi
+
+# сохранять пропорции при изменении окна [-]
+# бесшовную текстуру для солнца [-]
+# повернуть ось вращения планеты [-]
+# вращение луны по наклонной орбите а не по экватору [-]
+
+
+def draw_skybox_sphere(radius, slices, stacks, textures):
+    quadric = gluNewQuadric()
+    gluQuadricTexture(quadric, GL_TRUE)
+    gluQuadricNormals(quadric, GLU_SMOOTH)
+
+    for i in range(len(textures)):
+        glBindTexture(GL_TEXTURE_2D, textures[i])
+
+        glBegin(GL_QUADS)
+        for j in range(slices + 1):
+            lat0 = pi * (-0.5 + (i) / stacks)
+            z0 = sin(lat0)
+            zr0 = cos(lat0)
+
+            lat1 = pi * (-0.5 + (i + 1) / stacks)
+            z1 = sin(lat1)
+            zr1 = cos(lat1)
+
+            lng0 = 2 * pi * (j - 1) / slices
+            lng1 = 2 * pi * j / slices
+
+            x00 = cos(lng0) * zr0 * radius
+            y00 = sin(lng0) * zr0 * radius
+            z00 = z0 * radius
+
+            x01 = cos(lng1) * zr0 * radius
+            y01 = sin(lng1) * zr0 * radius
+            z01 = z0 * radius
+
+            x10 = cos(lng0) * zr1 * radius
+            y10 = sin(lng0) * zr1 * radius
+            z10 = z1 * radius
+
+            x11 = cos(lng1) * zr1 * radius
+            y11 = sin(lng1) * zr1 * radius
+            z11 = z1 * radius
+
+            glTexCoord2f(j / slices, i / stacks)
+            glVertex3f(x00, y00, z00)
+
+            glTexCoord2f((j + 1) / slices, i / stacks)
+            glVertex3f(x01, y01, z01)
+
+            glTexCoord2f((j + 1) / slices, (i + 1) / stacks)
+            glVertex3f(x11, y11, z11)
+
+            glTexCoord2f(j / slices, (i + 1) / stacks)
+            glVertex3f(x10, y10, z10)
+
+        glEnd()
+
+def draw_sphere(radius, slices, stacks):
+    quadric = gluNewQuadric()
+    gluQuadricTexture(quadric, GL_TRUE)
+    gluQuadricNormals(quadric, GLU_SMOOTH)
+    gluSphere(quadric, radius, slices, stacks)
+
+# def draw_sphere(radius, slices, stacks):
+#     for i in range(stacks):
+#         lat0 = pi * (-0.5 + (i) / stacks)
+#         z0 = sin(lat0)
+#         zr0 = cos(lat0)
+#
+#         lat1 = pi * (-0.5 + (i + 1) / stacks)
+#         z1 = sin(lat1)
+#         zr1 = cos(lat1)
+#
+#         # Start drawing strips
+#         glBegin(GL_QUAD_STRIP)
+#         for j in range(slices + 1):
+#             lng = 2 * pi * (j - 1) / slices
+#             x = cos(lng)
+#             y = sin(lng)
+#
+#             glTexCoord2f((j - 1) / slices, i / stacks)
+#             glVertex3f(x * zr0 * radius, y * zr0 * radius, z0 * radius)
+#
+#             glTexCoord2f((j - 1) / slices, (i + 1) / stacks)
+#             glVertex3f(x * zr1 * radius, y * zr1 * radius, z1 * radius)
+#
+#         glEnd()
+
+
+def load_texture(file_path):
+    image = Image.open(file_path)
+    width, height = image.size
+    image_data = image.tobytes("raw", "RGBX", 0, -1)
+    texture_id = glGenTextures(1)
+
+    glBindTexture(GL_TEXTURE_2D, texture_id)
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+
+    return texture_id
+
+
+def load_texture_skybox(filename):
+    image = Image.open(filename)
+    width, height = image.size
+    side_length = width // 4
+    textures = []
+
+    for i in range(6):
+        left = side_length * i
+        top = 0
+        right = side_length * (i + 1)
+        bottom = side_length
+        texture_image = image.crop((left, top, right, bottom))
+        texture_data = texture_image.tobytes("raw", "RGBX", 0, -1)
+        texture_id = glGenTextures(1)
+
+        glBindTexture(GL_TEXTURE_2D, texture_id)
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, side_length, side_length, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture_data)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+
+        textures.append(texture_id)
+
+    return textures
+
+
+class SolarSystemWidget(QOpenGLWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.sun_angle = 7.25
+        self.earth_angle = 23.5
+        self.moon_angle = 0
+        self.camera_distance = 10.0
+        self.camera_angle_x = 0.0
+        self.camera_angle_y = 0.0
+        self.last_mouse_pos = None
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_animation)
+        self.timer.start(30)
+        self.sun_texture = None
+        self.earth_texture = None
+        self.moon_texture = None
+        self.skybox_textures = None
+        self.skybox_size = 50.0
+
+    def initializeGL(self):
+        glClearColor(0.0, 0.0, 0.0, 1.0)
+        glEnable(GL_DEPTH_TEST)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(45, self.width() / self.height(), 0.1, 100.0)
+        glMatrixMode(GL_MODELVIEW)
+
+        self.sun_texture = load_texture(r"Textures\sun.png")
+        self.earth_texture = load_texture(r"Textures\earth.png")
+        self.moon_texture = load_texture(r"Textures\moon.jpg")
+        self.skybox_textures = load_texture_skybox(r"Textures\skybox.jpg")
+
+        for texture_id in self.skybox_textures:
+            glBindTexture(GL_TEXTURE_2D, texture_id)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+
+        light_position = [0.0, 0.0, 0.0, 1.0]
+        glLightfv(GL_LIGHT0, GL_POSITION, light_position)
+
+        light_ambient = [0.0, 0.0, 0.0, 1.0]
+        glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient)
+
+        light_diffuse = [1.0, 1.0, 1.0, 1.0]
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse)
+
+        light_specular = [1.0, 1.0, 1.0, 1.0]
+        glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular)
+
+    def resizeGL(self, w, h):
+        glViewport(0, 0, w, h)
+
+    def paintGL(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()
+
+        self.draw_skybox()
+        # Moving camera
+        glTranslatef(0.0, 0.0, -self.camera_distance)
+        glRotatef(self.camera_angle_x, 1.0, 0.0, 0.0)
+        glRotatef(self.camera_angle_y, 0.0, 1.0, 0.0)
+
+        # Sun
+        glEnable(GL_TEXTURE_2D)
+        glBindTexture(GL_TEXTURE_2D, self.sun_texture)
+        glColor3f(1.0, 1.0, 1.0)
+        draw_sphere(1.0, 50, 50)
+        glDisable(GL_TEXTURE_2D)
+
+        # Earth
+        glPushMatrix()
+        glRotatef(self.sun_angle, 0.0, 1.0, 0.0)
+        glTranslatef(5.0, 0.0, 0.0)
+        glRotatef(self.earth_angle, 0.0, 1.0, 0.0)
+
+        earth_ambient = [0.1, 0.1, 0.1, 1.0]
+        earth_diffuse = [0.5, 0.5, 0.5, 1.0]
+        earth_specular = [0.5, 0.5, 0.5, 1.0]
+        earth_shininess = 50.0
+        glMaterialfv(GL_FRONT, GL_AMBIENT, earth_ambient)
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, earth_diffuse)
+        glMaterialfv(GL_FRONT, GL_SPECULAR, earth_specular)
+        glMaterialf(GL_FRONT, GL_SHININESS, earth_shininess)
+
+        glEnable(GL_TEXTURE_2D)
+        glBindTexture(GL_TEXTURE_2D, self.earth_texture)
+        glColor3f(1.0, 1.0, 1.0)
+        draw_sphere(0.5, 50, 50)
+
+        # Moon
+        glPushMatrix()
+        glRotatef(self.moon_angle, 0.0, 1.0, 0.0)
+        glTranslatef(1.0, 0.0, 0.0)
+
+        moon_ambient = [0.1, 0.1, 0.1, 1.0]
+        moon_diffuse = [0.3, 0.3, 0.3, 1.0]
+        moon_specular = [0.3, 0.3, 0.3, 1.0]
+        moon_shininess = 20.0
+        glMaterialfv(GL_FRONT, GL_AMBIENT, moon_ambient)
+        glMaterialfv(GL_FRONT, GL_DIFFUSE, moon_diffuse)
+        glMaterialfv(GL_FRONT, GL_SPECULAR, moon_specular)
+        glMaterialf(GL_FRONT, GL_SHININESS, moon_shininess)
+
+        glBindTexture(GL_TEXTURE_2D, self.moon_texture)
+        draw_sphere(0.2, 50, 50)
+
+        glPopMatrix()
+        glPopMatrix()
+
+    def draw_skybox(self):
+        glPushMatrix()
+
+        glLoadIdentity()
+        glTranslatef(0, 0, -self.camera_distance)
+        glRotatef(self.camera_angle_x, 1, 0, 0)
+        glRotatef(self.camera_angle_y, 0, 1, 0)
+
+        glDisable(GL_DEPTH_TEST)
+        glEnable(GL_TEXTURE_2D)
+
+        # Front side
+        glBindTexture(GL_TEXTURE_2D, self.skybox_textures[0])
+        glBegin(GL_QUADS)
+        glTexCoord2f(0, 0)
+        glVertex3f(-50, -50, -50)
+        glTexCoord2f(1, 0)
+        glVertex3f(50, -50, -50)
+        glTexCoord2f(1, 1)
+        glVertex3f(50, 50, -50)
+        glTexCoord2f(0, 1)
+        glVertex3f(-50, 50, -50)
+        glEnd()
+
+        # Back side
+        glBindTexture(GL_TEXTURE_2D, self.skybox_textures[1])
+        glBegin(GL_QUADS)
+        glTexCoord2f(0, 0)
+        glVertex3f(-50, -50, 50)
+        glTexCoord2f(1, 0)
+        glVertex3f(50, -50, 50)
+        glTexCoord2f(1, 1)
+        glVertex3f(50, 50, 50)
+        glTexCoord2f(0, 1)
+        glVertex3f(-50, 50, 50)
+        glEnd()
+
+        # Upper side
+        glBindTexture(GL_TEXTURE_2D, self.skybox_textures[2])
+        glBegin(GL_QUADS)
+        glTexCoord2f(0, 1)
+        glVertex3f(-50, 50, -50)
+        glTexCoord2f(0, 0)
+        glVertex3f(-50, 50, 50)
+        glTexCoord2f(1, 0)
+        glVertex3f(50, 50, 50)
+        glTexCoord2f(1, 1)
+        glVertex3f(50, 50, -50)
+        glEnd()
+
+        # Downside
+        glBindTexture(GL_TEXTURE_2D, self.skybox_textures[3])
+        glBegin(GL_QUADS)
+        glTexCoord2f(1, 1)
+        glVertex3f(-50, -50, -50)
+        glTexCoord2f(1, 0)
+        glVertex3f(50, -50, -50)
+        glTexCoord2f(0, 0)
+        glVertex3f(50, -50, 50)
+        glTexCoord2f(0, 1)
+        glVertex3f(-50, -50, 50)
+        glEnd()
+
+        # Right side
+        glBindTexture(GL_TEXTURE_2D, self.skybox_textures[4])
+        glBegin(GL_QUADS)
+        glTexCoord2f(0, 0)
+        glVertex3f(-50, -50, -50)
+        glTexCoord2f(1, 0)
+        glVertex3f(-50, -50, 50)
+        glTexCoord2f(1, 1)
+        glVertex3f(-50, 50, 50)
+        glTexCoord2f(0, 1)
+        glVertex3f(-50, 50, -50)
+        glEnd()
+
+        # Left side
+        glBindTexture(GL_TEXTURE_2D, self.skybox_textures[5])
+        glBegin(GL_QUADS)
+        glTexCoord2f(0, 0)
+        glVertex3f(50, -50, -50)
+        glTexCoord2f(1, 0)
+        glVertex3f(50, -50, 50)
+        glTexCoord2f(1, 1)
+        glVertex3f(50, 50, 50)
+        glTexCoord2f(0, 1)
+        glVertex3f(50, 50, -50)
+        glEnd()
+
+        glEnable(GL_DEPTH_TEST)
+        glDisable(GL_TEXTURE_2D)
+
+        glPopMatrix()
+
+    def update_animation(self):
+        self.sun_angle += 0.1
+        self.earth_angle += 1.0
+        self.update()
+
+    def mousePressEvent(self, event):
+        if event.buttons() == Qt.LeftButton:
+            self.last_mouse_pos = event.pos()
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() == Qt.LeftButton:
+            if self.last_mouse_pos:
+                delta = event.pos() - self.last_mouse_pos
+                self.camera_angle_y += delta.x() / 5.0
+                self.camera_angle_x += delta.y() / 5.0
+                self.last_mouse_pos = event.pos()
+                self.update()
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.last_mouse_pos = None
+
+    def wheelEvent(self, event):
+        delta = event.angleDelta().y() / 120.0
+        self.camera_distance -= delta
+        self.update()
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(100, 100, 800, 600)
+        self.setWindowTitle("Sun, earth and moon")
+        self.central_widget = SolarSystemWidget(self)
+        self.setCentralWidget(self.central_widget)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
---
+++ lab5/task2/task2.py
@@ -0,0 +1,273 @@
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication, QMainWindow, QOpenGLWidget, QLabel
+from PyQt5.QtCore import QTimer, pyqtSignal, Qt
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from PIL import Image
+import random
+
+
+class GameWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.openglWidget = None
+        self.rotation_label = None
+        self.setWindowTitle("Memory Trainer 3D")
+        self.setGeometry(100, 100, 800, 600)
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.openglWidget = MyOpenGLWidget(self)
+        self.setCentralWidget(self.openglWidget)
+
+        self.rotation_label = QLabel(self)
+        self.rotation_label.setGeometry(10, 30, 200, 40)
+        self.rotation_label.setStyleSheet("background-color: rgba(255, 255, 255, 0.5);"
+                                          "border: 2px solid black;"
+                                          "color: black;")
+        self.rotation_label.setFont(QFont("Arial", 16))
+        self.rotation_label.setAlignment(Qt.AlignCenter)
+        self.rotation_label.setText("Rotations: 0")
+
+        menubar = self.menuBar()
+        game_menu = menubar.addMenu('Game Mode')
+
+        easy_action = game_menu.addAction('Easy (4x3)')
+        easy_action.triggered.connect(lambda: self.openglWidget.set_game_mode(4, 3))
+
+        medium_action = game_menu.addAction('Medium (6x5)')
+        medium_action.triggered.connect(lambda: self.openglWidget.set_game_mode(6, 5))
+
+        hard_action = game_menu.addAction('Hard (8x7)')
+        hard_action.triggered.connect(lambda: self.openglWidget.set_game_mode(8, 7))
+
+        self.openglWidget.rotation_count_changed.connect(self.update_rotation_label)
+
+    def update_rotation_label(self, count):
+        self.rotation_label.setText(f"Rotations: {count}")
+
+
+class MyOpenGLWidget(QOpenGLWidget):
+    rotation_count_changed = pyqtSignal(int)
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.grid_width = 4
+        self.grid_height = 3
+        self.texture_id = None
+        self.tile_states = [[False for _ in range(self.grid_width)] for _ in range(self.grid_height)]
+        self.clicked_tile = None
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.close_tiles)
+        self.animation_timer = QTimer(self)
+        self.animation_timer.timeout.connect(self.animate_tile_rotation)
+        self.animation_angle = 0
+        self.animation_duration = 500  # Duration of animation
+        self.animation_frame_interval = 16  # Frame moment
+        self.animation_frame_count = int(self.animation_duration / self.animation_frame_interval)
+        self.current_animation_frame = 0
+        self.tile_textures = {}
+        self.tile_numbers = {}
+        self.rotation_counts = {
+            (4, 3): 0,
+            (6, 5): 0,
+            (8, 7): 0
+        }
+
+    def load_all_tile_textures(self):
+        self.tile_textures = {}
+
+        for i in range(1, 17):
+            texture_path = f"Textures/tile_{i}.png"
+            texture_id = self.load_texture(texture_path)
+            if texture_id is not None:
+                self.tile_textures[f"tile_{i}"] = texture_id
+                self.tile_numbers[(i // 2, (i - 1) % 2)] = i
+            else:
+                print(f"Failed to load image: {texture_path}")
+
+    def initializeGL(self):
+        glClearColor(0.2, 0.2, 0.2, 1.0)
+        glEnable(GL_DEPTH_TEST)
+
+        self.texture_id = self.load_texture("Textures/back.png")
+        self.load_all_tile_textures()
+
+    def resizeGL(self, width, height):
+        glViewport(0, 0, width, height)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        aspect_ratio = width / height
+        gluPerspective(45, aspect_ratio, 0.1, 100)
+        glMatrixMode(GL_MODELVIEW)
+
+    def paintGL(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()
+        glTranslatef(0.0, 0.5, -10.0)
+        glRotatef(-30, 1, 0, 0)
+
+        tile_size = 1.0
+        gap = 0.1
+
+        grid_width = self.grid_width
+        grid_height = self.grid_height
+
+        if grid_width > self.width() or grid_height > self.height():
+            scale_factor = min(self.width() / grid_width, self.height() / grid_height)
+            tile_size *= scale_factor
+            gap *= scale_factor
+
+        start_pos_x = -((tile_size + gap) * grid_width) / 2.0
+        start_pos_y = -((tile_size + gap) * grid_height) / 2.0
+
+        glEnable(GL_TEXTURE_2D)
+
+        for i in range(grid_height):
+            for j in range(grid_width):
+                glPushMatrix()
+                if self.tile_states[i][j]:
+                    if (i, j) in self.tile_textures:
+                        glBindTexture(GL_TEXTURE_2D, self.tile_textures[(i, j)])
+                    else:
+                        glBindTexture(GL_TEXTURE_2D, self.texture_id)
+
+                    if self.clicked_tile == (i, j):
+                        # If tile open then use texture tile (doesn't work)
+                        # TODO: fix
+                        glBindTexture(GL_TEXTURE_2D, self.tile_textures[f"tile_{self.tile_numbers[(i, j)]}"])
+
+                        glTranslatef(start_pos_x + j * (tile_size + gap) + tile_size / 2,
+                                     start_pos_y + i * (tile_size + gap) + tile_size / 2, 0)
+                        glRotatef(self.animation_angle, 0, 1, 0)
+                        glTranslatef(-start_pos_x - j * (tile_size + gap) - tile_size / 2,
+                                     -start_pos_y - i * (tile_size + gap) - tile_size / 2, 0)
+                else:
+                    glBindTexture(GL_TEXTURE_2D, self.texture_id)
+
+                glBegin(GL_QUADS)
+                glTexCoord2f(0.0, 0.0)
+                glVertex3f(start_pos_x + j * (tile_size + gap), start_pos_y + i * (tile_size + gap), 0.0)
+                glTexCoord2f(1.0, 0.0)
+                glVertex3f(start_pos_x + (j + 1) * tile_size + j * gap, start_pos_y + i * (tile_size + gap), 0.0)
+                glTexCoord2f(1.0, 1.0)
+                glVertex3f(start_pos_x + (j + 1) * tile_size + j * gap, start_pos_y + (i + 1) * tile_size + i * gap,
+                           0.0)
+                glTexCoord2f(0.0, 1.0)
+                glVertex3f(start_pos_x + j * (tile_size + gap), start_pos_y + (i + 1) * tile_size + i * gap, 0.0)
+                glEnd()
+                glPopMatrix()
+
+        glDisable(GL_TEXTURE_2D)
+
+    def generate_image_deck(self, size):
+        num_tiles = size[0] * size[1]
+        num_pairs = num_tiles // 2
+
+        all_images = [f'Textures/tile_{i}.png' for i in range(1, num_pairs + 1)]
+        image_deck = all_images * 2
+
+        random.shuffle(image_deck)
+
+        return image_deck
+
+    def set_game_mode(self, width, height):
+        self.grid_width = width
+        self.grid_height = height
+        self.tile_states = [[False for _ in range(self.grid_width)] for _ in range(self.grid_height)]
+        self.rotation_counts[(self.grid_width, self.grid_height)] = 0
+        self.rotation_count_changed.emit(self.rotation_counts[(self.grid_width, self.grid_height)])
+        self.clicked_tile = None
+        self.timer.stop()
+        self.animation_timer.stop()
+        self.update()
+
+    def load_texture(self, file_path):
+        img = Image.open(file_path)
+        img_data = img.convert("RGBA").tobytes("raw", "RGBA")
+        texture_id = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, texture_id)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.width, img.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+        return texture_id
+
+    def mousePressEvent(self, event):
+        x = event.x()
+        y = event.y()
+
+        viewport = glGetIntegerv(GL_VIEWPORT)
+        win_x = x
+        win_y = viewport[3] - y
+        z = glReadPixels(win_x, win_y, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT)
+
+        win_x = x
+        win_y = viewport[3] - y
+        model_view = glGetDoublev(GL_MODELVIEW_MATRIX)
+        projection_view = glGetDoublev(GL_PROJECTION_MATRIX)
+        obj_x, obj_y, obj_z = gluUnProject(win_x, win_y, z[0][0], model_view, projection_view, viewport)
+
+        tile_size = 0.13
+        gap = 0.1
+
+        start_pos_x = -((tile_size + gap) * self.grid_width) / 2.0
+        start_pos_y = -((tile_size + gap) * self.grid_height) / 2.0
+
+        i = int((obj_y - start_pos_y) / (tile_size + gap))
+        j = int((obj_x - start_pos_x) / (tile_size + gap))
+
+        if 0 <= i < self.grid_height and 0 <= j < self.grid_width:
+            if not self.tile_states[i][j]:
+                # If tile doesn't open then rotate it
+                # TODO: fix cause it doesn't show when it has status open
+                self.rotation_counts[(self.grid_width, self.grid_height)] += 1
+                self.rotation_count_changed.emit(self.rotation_counts[(self.grid_width, self.grid_height)])
+                self.tile_states[i][j] = True
+                self.clicked_tile = (i, j)
+                self.animation_angle = 0
+                self.animation_timer.start(self.animation_frame_interval)
+                self.update()
+            else:
+                # If tile already open the close it
+                # TODO: fix
+                self.tile_states[i][j] = False
+                self.clicked_tile = None
+                self.current_animation_frame = 0
+                self.update()
+
+    def open_tile(self, i, j):
+        # Open tile fir cords (i, j) and set texture tile_i.png
+        # TODO: fix
+        self.rotation_counts[(self.grid_width, self.grid_height)] += 1
+        self.rotation_count_changed.emit(self.rotation_counts[(self.grid_width, self.grid_height)])
+        self.tile_states[i][j] = True
+        self.clicked_tile = (i, j)
+        self.animation_angle = 0
+        self.animation_timer.start(self.animation_frame_interval)
+        self.update()
+
+    def animate_tile_rotation(self):
+        self.animation_angle += 180 / self.animation_frame_count
+        self.current_animation_frame += 1
+        self.update()
+
+        if self.current_animation_frame >= self.animation_frame_count:
+            self.animation_timer.stop()
+            self.close_tiles()
+
+    def close_tiles(self):
+        if self.clicked_tile:
+            self.tile_states[self.clicked_tile[0]][self.clicked_tile[1]] = False
+            self.clicked_tile = None
+            self.current_animation_frame = 0
+            self.update()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = GameWindow()
+    window.show()
+    sys.exit(app.exec_())
